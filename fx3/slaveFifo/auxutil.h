e9e124073eb24ab6dac12230443c200ee22b6166
Reginald Eisenblatt <booyasdr@gmail.com>
2019-09-09 16:01:40 -0400

    delete fx3

--- fx3/slaveFifo/auxutil.h
+++
@@ -1,9 +0,0 @@
-#include "cyu3externcstart.h"
-//#include "cyu3types.h"
-#include "cyu3usbconst.h"
-#include "cyu3usb.h"
-
-void CyFxAppErrorHandler (  CyU3PReturnStatus_t apiRetStatus );
-CyBool_t CyFxApplnLPMRqtCB ( CyU3PUsbLinkPowerMode link_mode);
-void CyFxSlFifoApplnDebugInit (void);
-void setDescriptors(void);

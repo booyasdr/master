bcd3908094eabaa5120c513a443471282bfcd788
Reginald Eisenblatt <booyasdr@gmail.com>
2019-10-02 17:39:02 -0400

    rm fx2 to create its own repo

--- fx2/bench_dump/dataplot.m
+++
@@ -1,5 +0,0 @@
-%system('make','bench_dump')
-fid = fopen('datadump.dat');
-x = fread(fid,'ushort');
-
-plot(x),shg
--- fx2/dds/dds.m
+++
@@ -1,7 +0,0 @@
-format compact
-fdec = 7e6;
-df = 125e6/2^32;
-
-f = fdec/df;
-
-printf('%x\n',f)
